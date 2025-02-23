fcaa0c8aa7fb7fd5f1db000ca01664d5481befe0
dwarbler <92753567+dwarbler@users.noreply.github.com>
2025-02-22 20:10:00 -0600

    Refactor(GitGame): Refactor gitgame file for cleaner code and dynamic cloning

--- src/components/GitGame.tsx
+++ src/components/GitGame.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import Terminal from "./Terminal";
 import RepoVisualization from "./RepoVisualization";
 
+import initState from "@/app/states/init.json";
+
 interface File {
   filename: string;
   filetype: string;
@@ -27,6 +29,22 @@ interface RepoState {
   }[];
 }
 
+const recurseTree = (fn: (file: File) => void, files: File[]) => {
+  for (const file of files) {
+    fn(file);
+    if (file.files) {
+      recurseTree(fn, file.files);
+    }
+  }
+};
+
+const getCommitMessage = (args: string[]): string => {
+  return args
+    .slice(2)
+    .join(" ")
+    .slice(1, args.slice(2).join(" ").length - 1);
+};
+
 export default function GitGame() {
   const [repoState, setRepoState] = useState<RepoState>({
     branches: [],
@@ -40,167 +58,111 @@ export default function GitGame() {
     completed: false,
   });
 
-  const recurseTree = (fn: (file: File) => void, files: File[]) => {
-    for (const file of files) {
-      fn(file);
-      if (file.files) {
-        recurseTree(fn, file.files);
-      }
-    }
-  };
-
   const handleCommand = (command: string) => {
     // Parse and execute Git command
-    const [cmd, ...args] = command.split(" ");
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_cmd, ...args] = command.split(" ");
 
-    switch (cmd) {
-      case "git":
-        if (args[0] == "init") {
-          setRepoState({
-            branches: ["main"],
-            currentBranch: "main",
-            commits: [],
+    switch (args[0]) {
+      case "init":
+        setRepoState(initState);
+
+      case "branch":
+        const newBranch = args[1];
+        const fileTree = repoState.filetrees.find(
+          (filetree) => filetree.branch == repoState.currentBranch
+        );
+
+        if (!repoState.branches.includes(newBranch) && fileTree) {
+          setRepoState((prev) => ({
+            ...prev,
+            branches: [...prev.branches, newBranch],
             filetrees: [
+              ...prev.filetrees,
               {
-                branch: "main",
-                files: [
-                  {
-                    filename: ".git",
-                    filetype: "dir",
-                    staged: false,
-                  },
-                ],
+                branch: newBranch,
+                files: [...fileTree.files],
               },
             ],
-          });
-        } else if (repoState && args[0] === "branch") {
-          const newBranch = args[1];
-          if (!repoState.branches.includes(newBranch)) {
-            setRepoState((prev) => ({
-              ...prev,
-              branches: [...prev.branches, newBranch],
-            }));
-            if (
-              challenge.description ===
-              `Create a new branch called "${newBranch}"`
-            ) {
-              setChallenge((prev) => ({ ...prev, completed: true }));
-            }
-          }
-        } else if (args[0] == "log") {
-          setRepoState((prev) => ({
-            ...prev,
-            commits: [
-              ...prev.commits.filter(
-                (commit) => commit.branch != prev.currentBranch
-              ),
-              ...prev.commits
-                .filter((commit) => commit.branch == prev.currentBranch)
-                .map((commit) => ({ ...commit, logged: true })),
-            ],
-          }));
-        } else if (
-          args[0] == "clone" &&
-          args[1] == "git@github.com/gitBetterTester"
-        ) {
-          setRepoState({
-            branches: ["main"],
-            currentBranch: "main",
-            commits: [],
-            filetrees: [
-              {
-                branch: "main",
-                files: [
-                  {
-                    filename: ".git",
-                    filetype: "dir",
-                    staged: false,
-                  },
-                  {
-                    filename: "src",
-                    filetype: "dir",
-                    staged: false,
-                    files: [
-                      {
-                        filename: "app.tsx",
-                        filetype: "file",
-                        staged: false,
-                      },
-                      {
-                        filename: "globals.css",
-                        filetype: "file",
-                        staged: false,
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          });
-        } else if (args[0] == "add") {
-          const path = args[1].split("/");
-          // tree is modified inside of the recursive function
-          // however, eslint complains if it's not this way
-          // eslint-disable-next-line prefer-const
-          let tree = repoState.filetrees.find(
-            (filetree) => filetree.branch == repoState.currentBranch
-          );
-          if (path[0] == "." && path.length == 1) {
-            if (tree?.files) {
-              recurseTree((file: File) => {
-                file.staged = true;
-              }, tree?.files);
-            }
-          } else {
-            let curr = tree?.files.find((file) => file.filename == path[0]);
-
-            for (const file of path.slice(1)) {
-              curr = curr?.files?.find((f) => f.filename == file);
-            }
-
-            if (curr) {
-              curr.staged = true;
-            }
-          }
-
-          if (tree) {
-            setRepoState((prev) => ({
-              ...prev,
-              filetrees: [
-                ...prev.filetrees.filter(
-                  (filetree) => filetree.branch != repoState.currentBranch
-                ),
-                tree,
-              ],
-            }));
-          }
-        } else if (args[0] == "commit") {
-          setRepoState((prev) => ({
-            ...prev,
-            commits: [
-              ...prev.commits,
-              {
-                id: Math.random().toString(16).slice(2),
-                message: args
-                  .slice(2)
-                  .join(" ")
-                  .slice(1, args.slice(2).join(" ").length - 1),
-                branch: repoState.currentBranch,
-                pushed: false,
-                logged: false,
-              },
-            ],
-          }));
-        } else if (args[0] == "checkout") {
-          setRepoState((prev) => ({
-            ...prev,
-            currentBranch: args[1],
           }));
         }
-        // Add more Git command implementations here
-        break;
-      default:
-        console.log("Unknown command");
+
+      case "checkout":
+        setRepoState((prev) => ({
+          ...prev,
+          currentBranch: args[1],
+        }));
+
+      case "log":
+        setRepoState((prev) => ({
+          ...prev,
+          commits: [
+            ...prev.commits.filter(
+              (commit) => commit.branch != prev.currentBranch
+            ),
+            ...prev.commits
+              .filter((commit) => commit.branch == prev.currentBranch)
+              .map((commit) => ({ ...commit, logged: true })),
+          ],
+        }));
+
+      case "clone":
+        import(
+          `@/app/states/${args[1].split("/")[args[1].split("/").length - 1]}`
+        ).then((res) => setRepoState(res.default));
+
+      case "add":
+        const path = args[1].split("/");
+        // tree is modified inside of the recursive function
+        // however, eslint complains if it's not this way
+        // eslint-disable-next-line prefer-const
+        let tree = repoState.filetrees.find(
+          (filetree) => filetree.branch == repoState.currentBranch
+        );
+        if (path[0] == "." && path.length == 1) {
+          if (tree?.files) {
+            recurseTree((file: File) => {
+              file.staged = true;
+            }, tree?.files);
+          }
+        } else {
+          let curr = tree?.files.find((file) => file.filename == path[0]);
+
+          for (const file of path.slice(1)) {
+            curr = curr?.files?.find((f) => f.filename == file);
+          }
+
+          if (curr) {
+            curr.staged = true;
+          }
+        }
+
+        if (tree) {
+          setRepoState((prev) => ({
+            ...prev,
+            filetrees: [
+              ...prev.filetrees.filter(
+                (filetree) => filetree.branch != repoState.currentBranch
+              ),
+              tree,
+            ],
+          }));
+        }
+
+      case "commit":
+        setRepoState((prev) => ({
+          ...prev,
+          commits: [
+            ...prev.commits,
+            {
+              id: Math.random().toString(16).slice(2),
+              message: getCommitMessage(args),
+              branch: repoState.currentBranch,
+              pushed: false,
+              logged: false,
+            },
+          ],
+        }));
     }
   };
 
